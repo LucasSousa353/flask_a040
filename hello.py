9641d33e643a32e4924bf631f19e6b419ae110a8
LucasSousa353 <119136103+LucasSousa353@users.noreply.github.com>
2024-02-29 22:17:44 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 
 
 @app.errorhandler(404)
@@ -18,20 +20,23 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+@app.route('/user/<nome>/<pront>/<inst>')
+def ident(nome, pront, inst):
+
+    return render_template('ident.html', pront = pront, nome = nome, inst = inst)
+
+@app.route('/contextorequisicao/<nome>')
+def reqcontext(nome):
+    host_address = request.headers.get('User-Agent')
+    remote_ip = request.remote_addr
+    browser_data = request.host_url
+
+    return render_template('reqcontext.html', host_address=host_address, remote_ip=remote_ip, browser_data=browser_data, nome = nome)
 
 @app.route('/')
 def index():
-     return render_template('index.html', current_time=datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
 
-
-@app.route('/user/<name>/<pronto>/<facu>')
-def user(name, pronto, facu):
-    return render_template('user.html', name=name, pronto=pronto, facu=facu)
-
-
-@app.route("/contextorequisicao/<name>")
-def contex(name):
-    user_agent = request.headers.get('User-Agent')
-    url = request.remote_addr
-    ip = request.host_url
-    return render_template('contex.html', user_agent=user_agent, url=url, ip=ip, name=name)
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
